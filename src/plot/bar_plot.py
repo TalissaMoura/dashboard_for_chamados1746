a319ce84b9dffd1a7e0c1c37fc757640d319da2a
TalissaMoura <talissa.ayres@gmail.com>
2024-03-06 18:18:56 -0400

    add src/plot

---
+++ src/plot/bar_plot.py
@@ -0,0 +1,34 @@
+from typing import Any, Dict, Union
+
+import altair as alt
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def bar_plot(
+    dataframe: pd.DataFrame,
+    x: Union[str, alt.X],
+    y: Union[str, alt.Y],
+    encode_kw: Union[None, Dict[str, Any]] = None,
+    mark_bar_kw: Union[Dict[str, Any], None] = None,
+    config_xaxis: Union[None, Dict[str, Any]] = None,
+    config_yaxis: Union[None, Dict[str, Any]] = None,
+):
+
+    if encode_kw:
+        base_bar = alt.Chart(dataframe).encode(x=x, y=y, **encode_kw)
+    else:
+        base_bar = alt.Chart(dataframe).encode(
+            x=x,
+            y=y,
+        )
+
+    if config_xaxis:
+        base_bar = base_bar.configure_axisX(**config_xaxis)
+    elif config_yaxis:
+        base_bar = base_bar.configure_axisY(**config_yaxis)
+
+    if mark_bar_kw:
+        return base_bar.mark_bar(**mark_bar_kw)
+    else:
+        return base_bar.mark_bar()
